4d1879bd73bbe9b52f78846aadeced5a14d5aa2b
lpc <974123274@qq.com>
2017-05-18 20:53:38 +0800

    add something

--- chapter2/max_subsequence_sum.c
+++ chapter2/max_subsequence_sum.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-static int max3(int a, int b, int c) {
+static int max3(int a, int b, int c)
+{
 	int t = a > b ? a : b;
 	return t > c ? t : c;
 }
 
-static int max_sub_sum(int a[], int left, int right) {
+static int max_sub_sum(int a[], int left, int right)
+{
 	int max_left_sum, max_right_sum;
 	int max_left_border_sum, max_right_border_sum;
 	int left_border_sum, right_border_sum;
@@ -26,7 +28,7 @@ static int max_sub_sum(int a[], int left, int right) {
 	}
 
 	max_right_border_sum = 0, right_border_sum = 0;
-	for (i = center; i <= right; i++ ) {
+	for (i = center + 1; i <= right; i++ ) {
 		right_border_sum += a[i];
 		if (right_border_sum > max_right_border_sum)
 			max_right_border_sum = right_border_sum;
@@ -35,14 +37,34 @@ static int max_sub_sum(int a[], int left, int right) {
 	return max3(max_left_sum, max_right_sum, max_left_border_sum + max_right_border_sum);
 }
 
-int max_subsequence_sum(int a[], int n) {
+int max_subsequence_sum_1(int a[], int n)
+{
 	return max_sub_sum(a, 0, n - 1);
+}
+
+int max_subsequence_sum_2(int a[], int n)
+{
+	int this_sum, max_sum, i;
+
+	this_sum = max_sum = 0;
+	for (i = 0; i < n; ++i) {
+		this_sum += a[i];
+		if (this_sum > max_sum)
+			max_sum = this_sum;
+		else if (this_sum < 0)
+			this_sum = 0;
+
+	}
+
+	return max_sum;
 }
 
 static int test[] = {4, -3, 5, 2, -1, 2, 6, 2};
 int main(void)
 {
-	int sum = max_sub_sum(test, 0,7);
+	int sum = max_subsequence_sum_1(test, 8);
+	printf("max sum of subsequence is :%d\n", sum);
+	sum = max_subsequence_sum_2(test, 8);
 	printf("max sum of subsequence is :%d\n", sum);
 	return 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 Solutions for "Data Structures and Algorithm Analysis in C",Second Edition,Mark Allen Weiss 
+编译环境：windows 7 X64 sp1 + x86_64-w64-mingw32 5.1.0
+编译选项 -Wall -o3 -ansi
